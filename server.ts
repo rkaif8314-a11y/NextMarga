aa95c81cad1e2a5b3b6a8bb3d6d5bdfd0da61316
Kaif Ali <rkaif8314@gmail.com>
2026-09-24 22:26:03 +0530

    security: add browser permissions policy

--- server.ts
+++ server.ts
@@ -41,6 +41,7 @@ app.use((_req, res, next) => {
   res.setHeader("X-Content-Type-Options", "nosniff");
   res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
   res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
   next();
 });
 
